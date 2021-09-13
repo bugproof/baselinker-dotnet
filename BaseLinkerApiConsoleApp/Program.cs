3eab7a167d4ce717530ee63cbd0d18543709fd46
Konrad <konrad.kogut@outlook.com>
2021-09-13 13:07:06 +0200

    Update example console app. The client now throws exceptions by default. Old behavior can be enabled by setting ThrowExceptions to false.

--- BaseLinkerApiConsoleApp/Program.cs
+++ BaseLinkerApiConsoleApp/Program.cs
@@ -9,15 +9,16 @@ using Requests = BaseLinkerApi.Requests;
 
 using var httpClient = new HttpClient();
 var baseLinkerClient = new BaseLinkerApiClient(httpClient, "3001646-3007387-D7KZKSX8A2JFJEX41KG9YNBCQEPLYBHKRVSNM4QC0DWSN29226CZVVDSGR6A5WR6");
-var response = await baseLinkerClient.Send(new Requests.CourierShipments.GetCouriersList());
-if (response.IsSuccessStatus)
+// baseLinkerClient.ThrowExceptions = false; // Set this to false if you want to check success status manually, error-prone. If you forget to check you will get null reference exceptions.
+try
 {
+    var response = await baseLinkerClient.Send(new Requests.CourierShipments.GetCouriersList());
     foreach (var courier in response.Couriers)
     {
         Console.WriteLine(courier.Code);
     }
 }
-else
+catch (BaseLinkerException baseLinkerException)
 {
-    Console.WriteLine($"Error! {response.ErrorCode} - {response.ErrorMessage}");
+    Console.WriteLine($"Error! {baseLinkerException.ErrorCode} - {baseLinkerException.ErrorMessage}");
 }
