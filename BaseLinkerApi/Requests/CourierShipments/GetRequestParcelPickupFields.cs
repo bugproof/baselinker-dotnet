fe10b292c8317a2784566bac2d4dd66b80898ddf
Konrad <konrad.kogut@outlook.com>
2022-04-10 12:20:55 +0200

    Add parcel related methods Closes #11

---
+++ BaseLinkerApi/Requests/CourierShipments/GetRequestParcelPickupFields.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using BaseLinkerApi.Common;
+
+namespace BaseLinkerApi.Requests.CourierShipments;
+
+/// <summary>
+/// The method allows you to retrieve additional fields for a parcel pickup request.
+/// </summary>
+public class GetRequestParcelPickupFields : IRequest<GetRequestParcelPickupFields.Response>
+{
+    [JsonPropertyName("courier_code")]
+    public string CourierCode { get; set; }
+    
+    public class RequestParcelPickupField
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+    
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+    
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+    
+        [JsonPropertyName("desc")]
+        public string? Desc { get; set; }
+        
+        /// <summary>
+        /// List of available options (appears for select, checkbox).
+        /// </summary>
+        [JsonPropertyName("options")] 
+        public Dictionary<string, string>? Options { get; set; }
+
+        /// <summary>
+        /// Default value for a field
+        /// </summary>
+        [JsonPropertyName("value")] 
+        public string? Value { get; set; }
+    }
+
+    public class Response : ResponseBase
+    {
+        public List<RequestParcelPickupField> Fields { get; set; }
+    }
+}
---
+++ BaseLinkerApi/Requests/CourierShipments/RequestParcelPickup.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using BaseLinkerApi.Common;
+
+namespace BaseLinkerApi.Requests.CourierShipments;
+
+public class RequestParcelPickup : IRequest<RequestParcelPickup.Response>
+{
+    [JsonPropertyName("courier_code")]
+    public string CourierCode { get; set; }
+
+    /// <summary>
+    /// Array of shipments ID, optional if package_numbers was provided
+    /// </summary>
+    [JsonPropertyName("package_ids")]
+    public List<string>? PackageIds { get; set; }
+    
+    /// <summary>
+    /// Array of shipments number (consignment number), optional if package_ids was provided
+    /// </summary>
+    [JsonPropertyName("package_numbers")]
+    public List<string>? PackageNumbers { get; set; }
+    
+    /// <summary>
+    /// Courier API account id for the courier accounts retrieved from the request getCourierAccounts
+    /// </summary>
+    [JsonPropertyName("account_id")]
+    public int AccountId { get; set; }
+
+    /// <summary>
+    /// List of form fields retrieved from the request getRequestParcelPickupFields
+    /// </summary>
+    [JsonPropertyName("fields")] 
+    public List<GetRequestParcelPickupFields.RequestParcelPickupField> Fields { get; set; }
+    
+    public class Response : ResponseBase
+    {
+        /// <summary>
+        /// The parcel pickup number provided by the courier API
+        /// </summary>
+        [JsonPropertyName("pickup_number")]
+        public string PickupNumber { get; set; }
+    }
+}
