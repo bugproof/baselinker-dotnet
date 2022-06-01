88706d376da2a7453ee1a3ad806ee31dbd7bcc79
Konrad <konrad.kogut@outlook.com>
2022-06-01 16:55:47 +0200

    Add missing method updateInventoryProductsPrices

---
+++ BaseLinkerApi/Requests/ProductCatalog/UpdateInventoryProductsPrices.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using BaseLinkerApi.Common;
+
+namespace BaseLinkerApi.Requests.ProductCatalog;
+
+public class UpdateInventoryProductsPrices : IRequest<UpdateInventoryProductsPrices.Response>
+{
+    [JsonPropertyName("inventory_id")]
+    public int InventoryId { get; set; }
+
+    [JsonPropertyName("products")]
+    public Dictionary<string, decimal> ProductPrices { get; set; }
+
+    public class Response : ResponseBase
+    {
+        [JsonPropertyName("counter")]
+        public int Counter { get; set; }
+            
+        [JsonPropertyName("warnings")]
+        public Dictionary<string, string> Warnings { get; set; }
+    }
+}
