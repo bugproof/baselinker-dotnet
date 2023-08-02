9b1ceec0ed164ed246b75a247480417b6723c5cf
Jan Bartula - Militaria.pl <jbartula@militaria.pl>
2023-08-02 15:33:49 +0200

    Add missing fields, fix wrong data type

--- BaseLinkerApi/Requests/ProductCatalog/UpdateInventoryProductsStock.cs
+++ BaseLinkerApi/Requests/ProductCatalog/UpdateInventoryProductsStock.cs
@@ -14,20 +14,20 @@ public class UpdateInventoryProductsStock : IRequest<UpdateInventoryProductsStoc
     /// </summary>
     [JsonPropertyName("inventory_id")]
     public int InventoryId { get; set; }
-        
+
     /// <summary>
     /// An array of products, where the key is a product ID and the value is a list of stocks. When determining the variant stock, provide variant ID as a product ID.
     /// In the stocks array the key should be the warehouse ID and the value - stock for that warehouse. The format of the warehouse identifier should be as follows: "[type:bl|shop|warehouse]_[id:int]". (e.g. "bl_123"). The list of warehouse identifiers can be retrieved using the getInventoryWarehousesmethod.
     /// Stocks cannot be assigned to the warehouses created automatically for purposes of keeping external stocks (shops, wholesalers, etc.).
     /// </summary>
     [JsonPropertyName("products")]
-    public Dictionary<string, int> ProductStocks { get; set; }
+    public Dictionary<string, Dictionary<string, int>> ProductStocks { get; set; }
 
     public class Response : ResponseBase
     {
         [JsonPropertyName("counter")]
         public int Counter { get; set; }
-            
+
         [JsonPropertyName("warnings")]
         public Dictionary<string, string> Warnings { get; set; }
     }
