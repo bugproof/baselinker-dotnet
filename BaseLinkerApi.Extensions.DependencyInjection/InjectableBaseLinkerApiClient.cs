3aa6064ffcd24f1ebc1dd9adec5a4160dd7ea2c9
bugproof <3116731+bugproof@users.noreply.github.com>
2023-08-23 17:23:26 +0200

    Fix #17
    Rate limiting should now work correctly with dependency injection (static instance of a RateLimiter will be used)

--- BaseLinkerApi.Extensions.DependencyInjection/InjectableBaseLinkerApiClient.cs
+++ BaseLinkerApi.Extensions.DependencyInjection/InjectableBaseLinkerApiClient.cs
@@ -10,19 +10,23 @@ namespace BaseLinkerApi.Extensions.DependencyInjection;
 internal class InjectableBaseLinkerApiClient : IBaseLinkerApiClient
 {
     private readonly BaseLinkerApiClient _client;
+    private static FixedWindowRateLimiter? _rateLimiter;
 
     public InjectableBaseLinkerApiClient(HttpClient httpClient, IOptions<BaseLinkerOptions> options)
     {
         if (options.Value.Token == null) throw new ArgumentNullException(nameof(options.Value.Token));
+        
+        _rateLimiter ??= new FixedWindowRateLimiter(new FixedWindowRateLimiterOptions
+        {
+            Window = TimeSpan.FromMinutes(1),
+            PermitLimit = options.Value.MaxRequestsPerMinute
+        });
+        
         _client = new BaseLinkerApiClient(httpClient, options.Value.Token)
         {
             ThrowExceptions = options.Value.ThrowExceptions,
             UseRequestLimit = options.Value.UseRequestLimit,
-            TimeLimiter = new FixedWindowRateLimiter(new FixedWindowRateLimiterOptions
-            {
-                Window = TimeSpan.FromMinutes(1),
-                PermitLimit = options.Value.MaxRequestsPerMinute
-            })
+            TimeLimiter = _rateLimiter
         };
     }
 
